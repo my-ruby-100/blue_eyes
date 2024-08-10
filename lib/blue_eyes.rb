2366a577b38808cbaa0643a1e47cd8888b5e4cfa
guxiaobai <sikuan.gu@gmail.com>
2024-08-10 18:04:40 +0800

    first build

--- lib/blue_eyes.rb
+++ lib/blue_eyes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
-
+require 'socket'
 require_relative "blue_eyes/version"
 
 module BlueEyes
   class Error < StandardError; end
-  # Your code goes here...
+  class ParseError < Error; end
 end
